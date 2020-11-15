0d8c1d4ecd916ab19cfbeceee06e28705314179a
danielharmon <dannyaharmon@gmail.com>
2020-11-15 10:52:04 -0800

    Complete Client Index, Complete Server build

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+const express = require('express')
+const app = express();
+const port = 8080
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.get('/', (req, res) => {
+  console.log('Get request received')
+  console.log(req.body)
+  res.send('Hello World!')
+})
+app.post('/', (req, res) => {
+  console.log('Post Request Received', req)
+  //Convert to CSV and send back
+  res.send('Success')
+})
+
+app.listen(port, () => {
+  console.log(`CSV Generator is listening on port ${port}`)
+})
 
 
