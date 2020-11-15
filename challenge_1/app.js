512a407031db07e137b35e92a3f5b3f6f437bc7a
danielharmon <dannyaharmon@gmail.com>
2020-11-14 19:50:26 -0800

    Create HTML [WIP] for app.js

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,25 @@
+var nextPlayer = 'X'
+var changePlayer = function(){
+  if(this.innerHTML) {
+    return;
+  }
+  this.innerHTML = nextPlayer;
+  if (nextPlayer === 'X') {
+    nextPlayer = 'O'
+  } else {
+    nextPlayer = 'X'
+  }
+}
+var updateMessage = function(str) {
+  document.getElementByTagName('caption').innerHTML = str
+}
+var boxes = Array.from(document.getElementsByTagName('td'));
 
+
+boxes.forEach(element => element.addEventListener("click", changePlayer))
+document.getElementsByTagName('button')[0].addEventListener("click", function() {
+  boxes.forEach(element => element.innerHTML = "");
+  nextPlayer = 'X'
+  updateMessage(`Player ${nextPlayer}'s Turn`)
+})
 
