022628e6ee142a25b16599721abb7e3fa2b22121
danielharmon <dannyaharmon@gmail.com>
2020-11-15 11:51:00 -0800

    Form Sends JSON, server sets it on the req.body

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 8080
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   console.log('Get request received')
-  console.log(req.body)
+  console.log(req.body["JSON"])
   res.send('Hello World!')
 })
 app.post('/', (req, res) => {
-  console.log('Post Request Received', req)
+  console.log('Post Request Received', req.body["JSON"])
   //Convert to CSV and send back
   res.send('Success')
 })
