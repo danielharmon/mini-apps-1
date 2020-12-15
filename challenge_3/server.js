9c75e678903ee580fc1b972c41374d6b93bc39c5
danielharmon <dannyaharmon@gmail.com>
2020-12-14 22:42:30 -0800

    init package, create server, create basic html, setup babel, start react App

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+var express = require('express')
+var App = express();
 
+App.use(express.static('./public/index.html'))
+
+App.listen(1111, function() {
+  console.log('App is listening on port 1111')
+})
 
