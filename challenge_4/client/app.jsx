6222ab49077d1fed4db7d9aae646b043cd02c1c8
danielharmon <dannyaharmon@gmail.com>
2020-12-17 19:19:52 -0800

    Implement model/view/some functionality

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,17 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Board from './board.jsx'
+import './style.css'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+    <Board/>
+    )
+  }
+}
+ReactDOM.render(<App/>, document.getElementById('app'))
 
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+var express = require('express')
+var App = express()
+var bodyParser = require('body-parser')
 
+App.use(bodyParser.json())
+App.use(express.static("public"))
 
+App.listen(8080, ()=> {
+  console.log('App is listening on port 8080')
+})
